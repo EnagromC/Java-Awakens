250d35478d6f88ee0e43290a4db51f87c89994ee
BabaTheUnicorn <l.jeannot666@gmail.com>
2016-03-21 11:59:44 +0100

    Classe Plateau
    J'ai défini la classe Plateau. Il faut encore placer Zombie à la place
    de value dans le "put" du constructeur, et vérifier l'emploi de
    "contains" dans la méthode "estUneFlaque"

--- FinstereFlure/src/Model/Plateau.java
+++ FinstereFlure/src/Model/Plateau.java
@@ -5,10 +5,36 @@
  */
 package Model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Corentin
  */
 public class Plateau {
     
+    Map<int[],Pion> plateau = new HashMap<>(); //Contient les cases occupées du plateau.
+    ArrayList<Integer[]> flaques = new ArrayList<>(); //Contient les cases sur lesquelles il y a une flaque de sang.
+    
+    /**
+     * Plateau est le constructeur. Il place le zombie à sa case de départ, en [0;0]
+     */
+    public Plateau(){
+        int[] cle = new int[2];
+        cle[0] = 0;
+        cle[1] = 1;
+        this.plateau.put(cle, value) // value est à remplacer par Zombie
+    }
+    
+     /**
+      * estUneFlaque est une méthode qui regarde si la case en paramètre est une flaque ou pas.
+      * @param position case que l'on veut vérifier
+      * @return true si la case est une flaque
+      */
+    public boolean estUneFlaque(int[] position){
+        boolean flaque = this.flaques.contains(position);
+        return flaque;
+    }
 }
