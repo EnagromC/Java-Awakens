fc76e0b3b48720a12ddd4f5b4009cc993eb93c61
EnagromC <happycaelum@hotmail.fr>
2016-05-07 14:37:28 +0200

    Paquet et Carte
    J'ai créé les constructeurs des deux classes, et une méthode Piocher
    dans le paquet.

--- FinstereFlure/src/Model/Carte.java
+++ FinstereFlure/src/Model/Carte.java
@@ -10,5 +10,25 @@ package Model;
  * @author Morgane
  */
 public class Carte {
+     private int nombre ;
+     private boolean proie ; 
+    
+    /**
+     * C'est le constructeur !
+     * @param nombre : le nombre de proies ou de pas à manger/effectuer.
+     * @param proie : true si c'est une carte "proies", false si c'est une carte "pas".
+     */
+    public Carte(int nombre, boolean proie) {
+        this.nombre = nombre ;
+        this.proie = proie ;
+    }
+    
+    public int getNombre(){
+        return this.nombre ;
+    }
+    
+    public boolean getProie(){
+        return this.proie ;
+    }
     
 }
--- FinstereFlure/src/Model/Paquet.java
+++ FinstereFlure/src/Model/Paquet.java
@@ -5,10 +5,43 @@
  */
 package Model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  *
  * @author Morgane
  */
 public class Paquet {
+    ArrayList<Carte> paquet = new ArrayList<>() ;
+    
+    /**
+     * C'est le constructeur ! On ajoute chacune des cartes dans le paquet, puis on le mélange.
+     */
+    public Paquet() {
+        this.paquet.add(new Carte(5, false)) ;
+        this.paquet.add(new Carte(7, false)) ;
+        this.paquet.add(new Carte(7, false)) ;
+        this.paquet.add(new Carte(8, false)) ;
+        this.paquet.add(new Carte(8, false)) ;
+        this.paquet.add(new Carte(10, false)) ;
+        this.paquet.add(new Carte(1, true)) ;
+        this.paquet.add(new Carte(2, true)) ;
+        Collections.shuffle(this.paquet) ; //On mélange le paquet;
+        while (!this.paquet.get(0).getProie()) { //Tant que la première carte est une carte "proie", on remélange.
+            Collections.shuffle(this.paquet) ;
+        }
+    }
+    
+    /**
+     * On pioche une carte, et on la supprime du paquet.
+     * @return la carte piochée.
+     */
+    public Carte piocherCarte() {
+        Carte c = this.paquet.get(0) ;
+        this.paquet.remove(0) ;
+        return c ;
+    }
+    
     
 }
